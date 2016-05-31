259bf8f00b05f59e410f6ee5a45e9c9e92c1f231
Hsieh Yu-Hsuan <tom83615@gmail.com>
2016-06-01 00:55:28 +0800

    簡化顯示nav格式

--- www/js/pagemask.js
+++ www/js/pagemask.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
-    $("div.nav").html("<ul>\n" +
-                        "\t<li>\n"+
-                            "\t\t<a data-icon='"+'edit'+"' href='"+'#pageInpute'+"' data-transition='none'>\n"+
-                                "\t\t\t"+'輸入資料'+"\n"+
-                            "\t\t</a>\n"+
-                        "\t</li>\n"+
-                        "\t<li>\n"+
-                            "\t\t<a data-icon='"+'cloud'+"' href='"+'#pageShare'+"' data-transition='none'>\n"+
-                                "\t\t\t"+'分享 &amp; 儲存'+"\n"+
-                            "\t\t</a>\n"+
-                        "\t</li>\n"+
-                        "\t<li>\n"+
-                            "\t\t<a data-icon='"+'info'+"' href='"+'#pageOther'+"' data-transition='none'>\n"+
-                                "\t\t\t"+'其他'+"\n"+
-                            "\t\t</a>\n"+
-                        "\t</li>\n"+
-                    "</ul>");
+    //設定 nav 欄位
+    var navData = [] ;
+    navData[0] = ['edit','#pageInpute','輸入資料'];
+    navData[1] = ['cloud','#pageShare','分享 &amp; 儲存'];
+    navData[2] = ['info','#pageOther','其他'];
+
+    var str = "<ul>\n";
+    for(var i=0; i<navData.length; i++){
+        str = str + "\t<li>\n"+
+                        "\t\t<a data-icon='"+navData[i][1]+"' href='"+navData[i][2]+"' data-transition='none'>\n"+
+                            "\t\t\t"+navData[i][3]+"\n"+
+                        "\t\t</a>\n"+
+                    "\t</li>\n";
+    }
+    str = str + "<ul>";
+    $("div.nav").html(str);
 });
